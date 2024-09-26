08a0eedc3f47181747ff396235478e54807ef850
FacelessFresh <draguelo_tod@yahoo.com>
2024-09-25 20:32:33 -0500

    Update MousePanFix.lua
    This pretty much will now disable from dragging the mouse to pan the camera in game. It is at least somewhat functional now. It'll prevent the game from getting stuck in pan mode by preventing panning with the mouse altogether. I'm going to keep trying to get this to work better but I can guarantee this should keep it from getting stuck trying to pan the camera when you come back from checking that ffxi guide on bg-wiki or ffxiah.com. I'm just not sure how useful it'll be to anyone that utilizes panning the camera.

--- MousePanFix/MousePanFix.lua
+++ MousePanFix/MousePanFix.lua
@@ -10,24 +10,67 @@ _addon.author = 'Faceless'
 _addon.version = '8.675309'
 _addon.commands = {'mousepanfix', 'mpf'}
 
+-- Load necessary libraries at the top
+local logger = require('logger')
+local chat = require('chat')
+
 -- Track left mouse state to detect if it's stuck in pan mode
 local left_mouse_down = false  -- Variable to track left mouse button
---Sets Default Mode Types 
+-- Sets Default Mode Types 
 local LeftClick = 1
 local LeftRelease = 2 
---Debug Mode to Determine your Mouse Setup 
+-- Debug Mode to Determine your Mouse Setup 
 local debug_mode = false
--- Mouse event handler to detect clicks use the command //mpf set LeftClick # for left mouse button clicked or //mpf set LeftRelease # for left mouse button released.
---[[For My Logitech G520 Gaming Mouse
-		Type == 1 is Left Mouse Button is clicked
-		Type == 2 is Left Mouse Button is released
-		Type == 4 is Right Mouse Button is clicked
-		Type == 5 is Right Mouse Button is released
-		Type == 0 is Mouse is moved
-	]]
+local is_focused = true
+local mouse_update_timer = 0
+local focus_check_duration = 5 -- Time before considering focus lost
+
+-- Delay time before issuing LeftRelease on focus regain
+local release_delay = 0.1  -- In seconds (100ms)
+
+-- Get window settings
+local settings = windower.get_windower_settings()
+local window_x = settings.window_x_pos
+local window_y = settings.window_y_pos
+local window_width = settings.x_res
+local window_height = settings.y_res
+
+-- Focus check handlers
+local function simulate_left_release()
+    windower.send_command('/mouse ' .. LeftRelease)
+    left_mouse_down = false  -- Reset the left mouse down state
+end
+
+local function on_focus_lost()
+    if is_focused then
+        is_focused = false
+
+        -- Simulate left mouse button release to prevent stuck dragging
+        if left_mouse_down then
+            simulate_left_release()
+        end
+    end
+end
+
+local function on_focus_gained()
+    if not is_focused then
+        is_focused = true
+        
+        -- Force a left click and release after focus regained if the system thinks the left button is down
+        if left_mouse_down then
+            windower.send_command('/mouse ' .. LeftClick)
+            windower.send_command('/mouse ' .. LeftRelease)
+            left_mouse_down = false
+        else
+            windower.send_command('@wait ' .. release_delay .. '; input /mouse ' .. LeftRelease)
+        end
+    end
+end
 
 windower.register_event('mouse', function(type, x, y, delta, blocked)
+    -- Debug output for mouse event types
     if debug_mode then
+        windower.add_to_chat(8, "Mouse event type: " .. tostring(type))
         if type == 0 then
             windower.add_to_chat(8, "This action registers as type 0, to set this as either: for LeftClick type //mpf set leftclick 0 ; for LeftRelease type //mpf set leftrelease 0")
         elseif type == 1 then
@@ -42,29 +85,61 @@ windower.register_event('mouse', function(type, x, y, delta, blocked)
             windower.add_to_chat(8, "This action registers as type 5, to set this as either: for LeftClick type //mpf set leftclick 5 ; for LeftRelease type //mpf set leftrelease 5")
         end
     end
-	if type == LeftClick then  -- Mouse button pressed
-        if left_mouse_down then
-            left_mouse_down = false  -- Reset the state
-            return true  -- Block further processing
-        else
-            left_mouse_down = true  -- Set the flag when button is pressed
+
+    -- Track mouse movement within window boundaries
+    mouse_update_timer = 0  -- Reset the focus timer on mouse movement
+
+    -- Check mouse position and handle focus
+    if x >= window_x and x <= (window_x + window_width) and
+       y >= window_y and y <= (window_y + window_height) then
+        on_focus_gained()
+    else
+        on_focus_lost()
+    end
+
+    -- Handle mouse click and release
+    if type == LeftClick then
+        if not left_mouse_down then
+            left_mouse_down = true
         end
-
-    elseif type == LeftRelease then  											
-        left_mouse_down = false  -- Reset the flag on release
-    end
-end)
-
--- Check periodically to force reset if mouse is stuck
-windower.register_event('prerender', function()
-    if left_mouse_down then
+        return true
+    elseif type == LeftRelease then
         left_mouse_down = false
+        return true
+    end
+
+    return false  -- Allow other handlers to process the event
+end)
+
+-- Coroutine to check if focus is lost due to mouse inactivity
+windower.register_event('prerender', function()
+    mouse_update_timer = mouse_update_timer + 1
+
+    -- If the mouse has not moved for focus_check_duration, consider focus lost
+    if mouse_update_timer > focus_check_duration then
+        on_focus_lost()
     end
 end)
+
+-- Testing function
+local function test_mouse_click()
+    local success, err = pcall(function() 
+        logger.info('Testing mouse click...') 
+    end)
+
+    if not success then
+        -- Fallback to print if logger fails
+        print('Logger not initialized, using print: Testing mouse click...')
+        print('Error: ' .. err)
+    else
+        windower.send_command('/mouse ' .. LeftClick)  -- Simulate left click
+        windower.send_command('/mouse ' .. LeftRelease)  -- Simulate left release
+    end
+end
 
 -- Command handler for the addon
 windower.register_event('addon command', function(command, ...)
-	local args = {...}
+    local args = {...}
     command = command and command:lower() or 'help'
 
     if command == 'debug' then
@@ -82,14 +157,20 @@ windower.register_event('addon command', function(command, ...)
         else
             windower.add_to_chat(8, "Unknown set command. Use //mpf set leftclick <type> or //mpf set leftrelease <type>")
         end
-		
+
+    elseif command == 'test' then
+        test_mouse_click()  -- Call the test function
+
     elseif command == 'help' or command == 'h' then
         windower.add_to_chat(8, _addon.name .. ' v.' .. _addon.version)
         windower.add_to_chat(8, 'This addon prevents the mouse from getting stuck in pan mode.')
-         windower.add_to_chat(8, 'Use //mpf debug to toggle debug mode. Use This to determine your Mouse Mode')
-        windower.add_to_chat(8, 'Use //mpf set leftclick <#> to change the left click type. Default Type is Set to 1')
-        windower.add_to_chat(8, 'Use //mpf set leftrelease <#> to change the left release type. Default Type is Set to 2')
+        windower.add_to_chat(8, 'Use //mpf debug to toggle debug mode.')
+        windower.add_to_chat(8, 'Use //mpf set leftclick <#> to change the left click type. Default is 1.')
+        windower.add_to_chat(8, 'Use //mpf set leftrelease <#> to change the left release type. Default is 2.')
+        windower.add_to_chat(8, 'Use //mpf test to simulate a mouse click for testing.')
     else
-        windower.add_to_chat(8, 'Unknown command. Use //mousepanfix help for more information.')
+        windower.add_to_chat(8, 'Unknown command. Use //mpf help for more information.')
     end
 end)
+
+print('MousePanFix addon loaded. Monitoring focus and mouse input.')
